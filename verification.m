3b51bbfa68ffe2319cacb866603aac698a8fa776
adeibe <adeibe@udc.es>
2023-03-20 18:33:57 +0100

    Add files via upload

--- verification.m
+++ verification.m
@@ -1,23 +1,34 @@
-% Evaluates the TVKAE attitude estimation error in nine different
-% experimental scenarios. The computed errors are in Deibe et al., "A
-% Time–Varying Kalman Filter for Low–Acceleration Attitude Estimation",
-% submitted to IEEE TIM, 2022.
+% When using this file, please cite the following works:
+% [1] A. Deibe, J.A. Anton Nacimiento, J. Cardenal, and F. López Peña, "A
+%     Kalman Filter for Nonlinear Attitude Estimation Using Time Variable
+%     Matrices and Quaternions," Sensors, vol. 20, no. 23, p. 6731, Nov.
+%     2020, https://doi.org/10.3390/s20236731
+% [2] A. Deibe, J.A. Anton Nacimiento, J. Cardenal, and F. López Peña, "A
+%     Time–Varying Kalman Filter for Low–Acceleration Attitude Estimation",
+%     Measurement, 2023, https://doi.org/10.1016/j.measurement.2023.112729.
+% 
+% This script evaluates the TVKAE attitude estimation error in nine
+% different experimental scenarios. The computed errors are in Deibe et
+% al. [2]. For a complete explanation of the TVKAE algoritm, see also [1].
 %
-% For a complete explanation of the TVKAE algoritm, see Deibe et al., "A
-% Kalman filter for nonlinear attitude estimation using time 
-% matrices and quaternions", Sensors, vol. 20, no. 23, Nov. 2020,
-% https://doi.org/10.3390/s20236731, and Deibe et al. "A Time–Varying
-% Kalman Filter for Low–Acceleration Attitude Estimation", submitted to
-% IEEE TIM, 2022.
-%
-% For a detailed description of the experimental scenarios, see Caruso et
-% al., "Analysis of the Accuracy of Ten Algorithms for Orientation
-% Estimation Using Inertial and Magnetic Sensing under Optimal Conditions:
-% One Size Does Not Fit All", Sensors, vol. 21, no. 7, p. 2543, Apr. 2021,
-% https://doi.org/10.3390/s21072543, and Caruso et al., "Orientation
-% Estimation Through Magneto-Inertial Sensor Fusion: A Heuristic Approach
-% for Suboptimal Parameters Tuning", IEEE Sensors Journal, vol. 21, no. 3,
-% pp. 3408-3419, 2021, https://doi.org/10.1109/JSEN.2020.3024806.
+% For a detailed description of the experimental scenarios, see:
+% Caruso et al., "Analysis of the Accuracy of Ten Algorithms for
+% Orientation Estimation Using Inertial and Magnetic Sensing under Optimal
+% Conditions: One Size Does Not Fit All", Sensors, vol. 21, no. 7, p. 2543,
+% Apr. 2021, doi.org/10.3390/s21072543,
+% and:
+% Caruso et al., "Orientation Estimation Through Magneto-Inertial Sensor
+% Fusion: A Heuristic Approach for Suboptimal Parameters Tuning", IEEE
+% Sensors Journal, vol. 21, no. 3, pp. 3408-3419, 2021,
+% doi.org/10.1109/JSEN.2020.3024806.
+
+% Input parameters:
+%                        +----------------------> stDevIn
+%                        |            +---------> IMUIndex
+%                        |            | +-------> SpeedIndex
+%                        |            | | +-----> Beta
+%              ----------+----------  | | | +---> Verbose
+%errEvalTVKAE([5.1174e-03 6.0706e-07],1,1,0,1);
 
 % IMU XSens
 errEvalTVKAE([5.1174e-03 6.0706e-07],1,1,0,1); % Slow
